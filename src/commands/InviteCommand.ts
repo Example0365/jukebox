04f5ae3a20eb7d75fd89fa4fced66159a87feee9
Example0365 <82190931+Example0365@users.noreply.github.com>
2021-04-09 10:29:12 +0800

    Update InviteCommand.ts

--- src/commands/InviteCommand.ts
+++ src/commands/InviteCommand.ts
@@ -15,9 +15,9 @@ export class InviteCommand extends BaseCommand {
         message.channel.send(
             createEmbed("info")
                 .setTimestamp()
-                .setTitle(`PlayEX Discord Bot Website.`)
+                .setTitle(`PlayEX Discord Bot Website`)
                 .setFooter(`Command Website Was Requested/\Executed By: ${message.author.tag}`, message.author.displayAvatarURL())
-                .addField("PlayEX Discord Bot Website", `[Click here](https://example0365.000webhostapp.com/)`)
+                .addField("PlayEX Discord Bot Website Link", `[Click here to open the website](https://example0365.000webhostapp.com/)`)
                 .setColor("#000000")
         ).catch(e => this.client.logger.error("PLAY_CMD_ERR:", e));
     }
